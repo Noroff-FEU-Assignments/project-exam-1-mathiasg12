02a393596672dad25cfeb8eae306a4d6bee61ff1
mathiasg12 <mathiasgausl@gmail.com>
2023-04-28 12:35:21 +0200

    v0.3
    header finished started js

---
+++ js/function.js
@@ -0,0 +1,26 @@
+const bars = document.querySelector(".fa-bars");
+const nav = document.querySelector("nav");
+const header = document.querySelector("header");
+const logo = document.querySelector(".logo");
+const logoCon = document.querySelector(".logoCon");
+const body = document.querySelector("main");
+const navList= document.querySelector("nav ul li")
+bars.addEventListener("click", () => {
+  nav.classList.toggle("navOn");
+  bars.classList.toggle("fa-x");
+  bars.classList.toggle("fa-bars");
+});
+document.addEventListener("scroll", () => {
+  if (window.scrollY >= 200) {
+    header.classList.add("smallHeader");
+    logo.classList.add("smallLogo");
+    logoCon.classList.add("smallLogoCon");
+    navList.classList.add("smallList")
+  } else {
+    header.classList.remove("smallHeader");
+    logo.classList.remove("smallLogo");
+    logoCon.classList.remove("smallLogoCon");
+    navList.classList.remove("smallList")
+  }
+});
+
